867e4d4329dd8a1c7b68496c72e41d3b14a52a5f
Connor McArthur <cmcarthur@rjmetrics.com>
2017-08-11 10:28:41 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-appointment-plus',
+      version='0.0.1',
+      description='Singer.io tap for Appointment Plus (appointment-plus.com)',
+      author='Fishtown Analytics',
+      url='http://fishtownanalytics.com',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_appointment_plus'],
+      install_requires=[
+          'singer-python>=1.5.0',
+          'voluptuous>=0.10.5',
+          'requests>=2.18.3',
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-appointment-plus=tap_appointment_plus:main
+      ''',
+      packages=['tap_appointment_plus'])
